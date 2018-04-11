d386fa9a44d270dfedf6b6d22920626414acb362
David Guare <36214309+dguare@users.noreply.github.com>
2018-04-11 18:05:46 -0400

    New Branch Commit

---
+++ Assets/Standard Assets/Characters/ThirdPersonCharacter/Scripts/CameraController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+	public Transform ethan;
+
+	private Vector3 offset;
+
+    [Range(0.01f, 1.0f)]
+    public float SmoothFactor = 0.5f;
+	// Use this for initialization
+	void Start () {
+		offset = transform.position - ethan.position;
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+        Vector3 newPos = ethan.position + offset;
+
+        transform.position = Vector3.Slerp(transform.position, newPos, SmoothFactor);
+	}
+}
