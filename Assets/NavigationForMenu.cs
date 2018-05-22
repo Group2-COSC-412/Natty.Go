cb78f4626175c62ac17c10ece9ead7d67e656ea6
Adam Lynch <alynch14@students.towson.edu>
2018-05-22 16:25:55 -0400

    Added animal spawn points. Just need to add collision detection.

---
+++ Assets/NavigationForMenu.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class NavigationForMenu : MonoBehaviour {
+    int sceneNum;
+    public Button button;
+    SceneManager loader;
+    // Use this for initialization
+    void Start()
+    {
+        sceneNum = 0;
+        button = button.GetComponent<Button>();
+        loader = new SceneManager();
+        button.onClick.AddListener(ChangeScene);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void ChangeScene()
+    {
+        SceneManager.LoadScene("MainScene");
+
+    }
+}
--- Assets/Standard Assets/Characters/ThirdPersonCharacter/Scripts/AnimalMovement.cs
+++ Assets/Standard Assets/Characters/ThirdPersonCharacter/Scripts/AnimalMovement.cs
@@ -4,17 +4,71 @@ using UnityEngine;
 
 public class AnimalMovement : MonoBehaviour {
 
-    public float runSpeed;
-    public Vector3 direction;
-    public Vector3 newDirection;
+    public Vector3 velocity;
+    public float xVel;
+    public int MAX_SPEED = 2;
+    public int MOVEMENT_SPEED = 10;
+    public float MOVEMENT_FRICTION = 0.8f;
+    public float yVel;
+    public int var;
+    public float uVar;
+    public float horizontalMovement;
+    public float verticalMovement;
+    public float timeBetweenDirectionChange = 10f;
 
 	// Use this for initialization
 	void Start () {
-		
+        velocity = new Vector3(xVel, 0, yVel);
+        Vector3 move = (Vector3.left * yVel) + (Vector3.right * xVel);
+        gameObject.transform.Translate(move);
+
+        InvokeRepeating("Run", timeBetweenDirectionChange, timeBetweenDirectionChange);
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+
+    void Run()
+    {
+        horizontalMovement = Random.Range(-1, 1);
+        verticalMovement = Random.Range(-1, 1);
+
+        xVel += horizontalMovement * MOVEMENT_SPEED * Time.deltaTime;
+        yVel += verticalMovement * MOVEMENT_SPEED * Time.deltaTime;
+
+        if (horizontalMovement == 0)
+        {
+            xVel -= Time.deltaTime * MOVEMENT_FRICTION * Mathf.Sign(xVel);
+        }
+        if (verticalMovement == 0)
+        {
+            yVel -= Time.deltaTime * MOVEMENT_FRICTION * Mathf.Sign(yVel);
+        }
+
+        if (Mathf.Abs(xVel) > MAX_SPEED)
+        {
+            xVel = MAX_SPEED * Mathf.Sign(xVel);
+        }
+        else if (Mathf.Abs(xVel) < 0.1f)
+        {
+            xVel = 0;
+        }
+
+        if (Mathf.Abs(yVel) > MAX_SPEED)
+        {
+            yVel = MAX_SPEED * Mathf.Sign(yVel);
+        }
+        else if (Mathf.Abs(yVel) < 0.1f)
+        {
+            yVel = 0;
+        }
+
+
+        Vector3 move = (Vector3.left * yVel) + (Vector3.right * xVel);
+        //move.Normalize();
+
+        gameObject.transform.Translate(move);
+    }
 }
